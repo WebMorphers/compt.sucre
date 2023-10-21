1d12f2b6e41a71b46dbc296c45c971f5aedca027
Moncef Souri <145851559+WebMorphers@users.noreply.github.com>
2023-10-21 12:20:14 -0700

    dd

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,78 +1,119 @@
-import { useState } from "react";
-import logo from '../../assets/logo.png'
+import logo from "../../assets/logo.png";
+import { useEffect, useState } from "react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
-export default function NavBar() {
-    const [navbar, setNavbar] = useState(false);
+const Navbar = () => {
+  const [nav, setnav] = useState(false);
+  function chang() {
+    setnav(!nav);
+  }
+  const scrollToSection = (sectionId: string) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      window.scrollTo({
+        top: section.offsetTop,
+        behavior: "smooth",  
+      });
+    }
+  };
+  useEffect(() => {
+    const internalLinks = document.querySelectorAll('a[href^="#"]');
+    internalLinks.forEach((link) => {
+      link.addEventListener("click", (e) => {
+        e.preventDefault();
+        const targetId = link.getAttribute("href")?.slice(1);
+        if (targetId) {
+          scrollToSection(targetId);
+        }
+      });
+    });
+  }, []);
 
-    return (
-        <nav className="w-full   ">
-            <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
-                <div>
-                    <div className="flex items-center justify-between py-3 md:py-5 md:block">
-                    <img className="h-20 w-23" src={logo} alt="" /> 
+  return (
+    <div id="nav" className="flex w-full  top-0 fixed justify-between items-center h-24 mx-auto px-4 text-white">
+      <a href="#" onClick={() => scrollToSection("hello")}>
+          <img className="h-20 w-23" src={logo} alt="" />
+        </a>
+      <ul className="hidden md:flex">
+        <li className="p-4">
+          <a
+            className="no-underline mx-4 cursor-pointer font-light text-white hover:text-orange-600"
+            onClick={() => scrollToSection("about_us")}
+            href="#about_us"
+          >
+            ABOUT US
+          </a>
+        </li>
+        <li className="p-4">
+          <a
+            className="no-underline mx-4 cursor-pointer font-thin text-white hover:text-orange-600"
+            onClick={() => scrollToSection("projects")}
+            href="#projects"
+          >
+            OUR PROJECTS
+          </a>
+        </li>
+      </ul>
+      <ul className="hidden md:flex">
+        <li className="p-4">
+          {" "}
+          <a
+            className="no-underline mx-4 cursor-pointer font-normal	text-white hover:text-orange-600"
+            onClick={() => scrollToSection("contact")}
+            href="#contact"
+          >
+            CONTACT US
+          </a>
+        </li>
+      </ul>
+      <div onClick={chang} className="block md:hidden">
+        {nav ? (
+          <AiOutlineClose size={20} color={"white"}/>
+        ) : (
+          <AiOutlineMenu size={20} color={"white"}/>
+        )}
+      </div>
+      <ul
+        className={
+          nav
+            ? "fixed left-0 top-0  h-full w-[90%] border-r border-r-gray-900 bg-amber-700 ease-in-out duration-500 flex items-center justify-center flex-col"
+            : "ease-in-out duration-500 fixed left-[-100%]"
+        }
+      >
+         <a href="#" onClick={() => scrollToSection("hello")}>
+          <img className="h-20 w-23" src={logo} alt="" />
+        </a>
+        <li className="p-4 ">
+          <a
+            className="no-underline mx-4 cursor-pointer font-thin text-white hover:text-orange-600"
+            onClick={() => scrollToSection("about_us")}
+            href="#about_us"
+          >
+            ABOUT US
+          </a>
+        </li>
+        <li className="p-4">
+          <a
+            className="no-underline mx-4 cursor-pointer font-thin text-white hover:text-orange-600 "
+            onClick={() => scrollToSection("projects")}
+            href="#projects"
+          >
+            OUR PROJECTS
+          </a>
+        </li>
+        <li className="p-4">
+          {" "}
+          <a
+            className="no-underline mx-4 cursor-pointer font-thin text-white hover:text-orange-600 "
+            onClick={() => scrollToSection("contact")}
+            href="#contact"
+          >
+            CONTACT US
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-
-                        <div className="md:hidden">
-                            <button
-                                className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
-                                onClick={() => setNavbar(!navbar)}
-                            >
-                                {navbar ? (
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        className="w-6 h-6"
-                                        viewBox="0 0 20 20"
-                                        fill="currentColor"
-                                    >
-                                        <path
-                                            fillRule="evenodd"
-                                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                            clipRule="evenodd"
-                                        />
-                                    </svg>
-                                ) : (
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        className="w-6 h-6"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        stroke="currentColor"
-                                        strokeWidth={2}
-                                    >
-                                        <path
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            d="M4 6h16M4 12h16M4 18h16"
-                                        />
-                                    </svg>
-                                )}
-                            </button>
-                        </div>
-                    </div>
-                </div>
-                <div>
-                    <div
-                        className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 bg-stone-600 ${
-                            navbar ? "block" : "hidden"
-                        }`}
-                    >
-                        <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
-                            <li className="text-white    hover:text-gray-400 duration-500">
-                                <a href="javascript:void(0)">Home</a>
-                            </li>
-                            <li className="text-white    hover:text-gray-400 duration-500">
-                                <a href="javascript:void(0)">Blog</a>
-                            </li>
-                            <li className="text-white    hover:text-gray-400 duration-500">
-                                <a href="javascript:void(0)">About US</a>
-                            </li>
-                            <li className="text-white    hover:text-gray-400 duration-500">
-                                <a href="javascript:void(0)">Contact US</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </nav>
-    );
-}
+export default Navbar;
