77a5a68e204fb30da9669cbcd89f3928c4160da3
othsoh <sohabsouri@gmail.com>
2023-10-21 16:29:05 +0100

    t

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,7 @@ const App = () => {
 
   return (
     <>
-      <div className='relative z-0 bg-primary'>
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
-          <Navbar />
-          <Hero />
-        </div>
-       
+      <div className=''>
         </div>
     </>
   )
