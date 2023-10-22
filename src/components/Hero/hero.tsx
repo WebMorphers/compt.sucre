90bbf65e26a5e48982ee80711bd312230156a0eb
othsoh <sohabsouri@gmail.com>
2023-10-22 12:41:46 +0100

    t

--- src/components/Hero/hero.tsx
+++ src/components/Hero/hero.tsx
@@ -14,10 +14,10 @@ const Hero = () => {
     <div className="hero w-full relative z-0 flex flex-col justify-center items-center" style={heroStyle}>
       <Navbar />
       
-      <div className='flex flex-col justify-center items-center text-center gap-7'>
+      <div className='flex flex-col justify-center items-center text-center gap-7 px-11 mt-24'>
         <h1 id='h1' className='text-3xl text-white'>Home-baked with love</h1>
         <div className="bg-[#FFF] h-10 w-[2px]"></div>
-        <p id='pp' className='text-xl text-white max-w-xl'>
+        <p id='pp' className='text-xl text-white max-w-xl font-extralight'>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
