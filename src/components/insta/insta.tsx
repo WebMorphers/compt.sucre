28064c5cd49f790f5aa8c4082bb34ca0eb948f4d
Moncef Souri <145851559+WebMorphers@users.noreply.github.com>
2023-10-22 07:37:59 -0700

    2

---
+++ src/components/insta/insta.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const insta = () => {
+  return (
+    <div>
+        <h1 className='text-sky-950'>Suivez  <a href="" className='visited:text-black  hover:text-neutral-700 text-inherit'><span className='underline'>@killerbrownie</span></a> sur Instagram pour plus d'inspiration en brownies gourmets !</h1>
+        <div className='grid grid-cols-'>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default insta
