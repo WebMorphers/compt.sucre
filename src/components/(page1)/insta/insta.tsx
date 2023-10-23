358c3e180b1aa1bb3dddb42cc5f1b61c7501d7e7
moncef <145851559+WebMorphers@users.noreply.github.com>
2023-10-23 22:42:08 +0000

    d

--- src/components/(page1)/insta/insta.tsx
+++ src/components/(page1)/insta/insta.tsx
@@ -1,44 +1,54 @@
-import HoverVideoPlayer from 'react-hover-video-player'
-import vid1 from '../../../assets/insta1.mp4'
-import vid2 from '../../../assets/insta2.mp4'
-import vid3 from '../../../assets/insta3.mp4'
-import vid4 from '../../../assets/insta4.mp4'
-import "./insta.css"
-import LazyLoad from 'react-lazy-load';
-
-
+import HoverVideoPlayer from "react-hover-video-player";
+import vid1 from "../../../assets/insta1.mp4";
+import vid2 from "../../../assets/insta2.mp4";
+import vid3 from "../../../assets/insta3.mp4";
+import vid4 from "../../../assets/insta4.mp4";
+import "./insta.css";
+import LazyLoad from "react-lazy-load";
 
 const insta = () => {
   return (
-    
+    <div className="py-8 px-20 max-sm:px-12 xl:px-36 flex flex-col justify-center items-center gap-5">
+      <h1 className="text-6xl title text-center">Taste Like Never Before .</h1>
 
-    <div className='py-8 px-20 max-sm:px-12 xl:px-36 flex flex-col justify-center items-center gap-5'>
-    <h1 className='text-6xl title text-center'>Taste Like Never Before .</h1>
+      <h1 className="text-sky-950 text-center">
+        Suivez{" "}
+        <a
+          href="https://www.instagram.com/comptoir.sucre/"
+          target="-blank"
+          className="visited:text-black  hover:text-neutral-700 text-inherit"
+        >
+          <span className="underline">@comptoir.sucre</span>
+        </a>{" "}
+        sur Instagram pour plus d'inspiration en brownies gourmets !
+      </h1>
+      <div className="grid md:grid-cols-4 gap-9 grid-cols-2">
+        <HoverVideoPlayer
+          loadingOverlay={<div className="loading-overlay">Loading...</div>}
+          restartOnPaused
+          videoSrc={vid1}
+        />
 
-        <h1 className='text-sky-950 text-center'>Suivez  <a href="https://www.instagram.com/comptoir.sucre/" target='-blank' className='visited:text-black  hover:text-neutral-700 text-inherit'><span className='underline'>@comptoir.sucre</span></a> sur Instagram pour plus d'inspiration en brownies gourmets !</h1>
-        <div className='grid md:grid-cols-4 gap-9 grid-cols-2'>
-        <LazyLoad threshold={1} >
-    
-        <HoverVideoPlayer restartOnPaused  videoSrc={vid1}/>
-        </LazyLoad>
-        <LazyLoad  threshold={1}>
-    
-        <HoverVideoPlayer restartOnPaused  videoSrc={vid2}/>
-        </LazyLoad>
-        <LazyLoad threshold={1} >
-    
-        <HoverVideoPlayer  restartOnPaused videoSrc={vid3}/>
-        </LazyLoad>
-        <LazyLoad threshold={1} >
-    
-        <HoverVideoPlayer restartOnPaused   videoSrc={vid4}/>
-        </LazyLoad>
+        <HoverVideoPlayer
+          loadingOverlay={<div className="loading-overlay">Loading...</div>}
+          restartOnPaused
+          videoSrc={vid2}
+        />
 
-        </div>
+        <HoverVideoPlayer
+          loadingOverlay={<div className="loading-overlay">Loading...</div>}
+          restartOnPaused
+          videoSrc={vid3}
+        />
 
+        <HoverVideoPlayer
+          restartOnPaused
+          loadingOverlay={<div className="loading-overlay">Loading...</div>}
+          videoSrc={vid4}
+        />
+      </div>
     </div>
-  )
-  
-}
+  );
+};
 
-export default insta
+export default insta;
