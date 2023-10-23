5f2d961a7741ea64ab40f6c7b2c1a24c62bdad9b
moncef <145851559+WebMorphers@users.noreply.github.com>
2023-10-23 21:29:09 +0000

    Update menu.tsx

--- src/routes/menu.tsx
+++ src/routes/menu.tsx
@@ -1,63 +1,36 @@
-
+import data from '../data.json'
 import img from '../assets/cake1.png'
-
-
-
+ 
 import Navbar from "../components/(page2)/navbar/navbar"
 
 const menu = () => { 
-  return (
-    <>
-            <Navbar />
-            <div className='pt-24'>
-                <div>
-                    <div className='flex justify-center items-center flex-wrap'>
-                        <div className='m-10 flex flex-col justify-center items-center p-5 px-10 gap-5 flex-wrap shadow-xl rounded-3xl hover:shadow-2xl cursor-pointer max-w-sm'>
-                            <div className='bg-gradient-to-t from-gray-300 to-gray-500 w-full flex justify-center p-5'>
-                                <img src={img} alt="Cake"/>
-                            </div>
-                            <div className='flex flex-col items-center justify-center text-start gap-3'>
-                                <div className='text-2xl font-bold '>
-                                    <h1>cake 1</h1>
-                                </div>
-                                <div className='font-light '>
-                                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam molestias non, sapiente dolores ipsa maxime inventore omnis dolorum, libero dicta voluptatem nisi facilis modi porro tenetur et itaque recusandae! Delectus.
-                                </div>
-                            </div>
-
-                        </div>
-                        <div className='m-10 flex flex-col justify-center items-center p-5 px-10 gap-5 flex-wrap shadow-xl rounded-3xl  hover:shadow-2xl cursor-pointer max-w-sm'>
-                            <div className='bg-gradient-to-t from-gray-300 to-gray-500 w-full flex justify-center p-5'>
-                                <img src={img} alt="Cake"/>
-                            </div>
-                            <div className='flex flex-col items-center justify-center text-start gap-3'>
-                                <div className='text-2xl font-bold '>
-                                    <h1>cake 1</h1>
-                                </div>
-                                <div className='font-light '>
-                                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam molestias non, sapiente dolores ipsa maxime inventore omnis dolorum, libero dicta voluptatem nisi facilis modi porro tenetur et itaque recusandae! Delectus.
-                                </div>
-                            </div>
-
-                        </div><div className='m-10 flex flex-col justify-center items-center p-5 px-10 gap-5 flex-wrap shadow-xl rounded-3xl hover:shadow-2xl cursor-pointer max-w-sm'>
-                            <div className='bg-gradient-to-t from-gray-300 to-gray-500 w-full flex justify-center p-5'>
-                                <img src={img} alt="Cake"/>
-                            </div>
-                            <div className='flex flex-col items-center justify-center text-start gap-3'>
-                                <div className='text-2xl font-bold flex justify-center'>
-                                    <h1>cake 1</h1>
-                                </div>
-                                <div className='font-light '>
-                                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam molestias non, sapiente dolores ipsa maxime inventore omnis dolorum, libero dicta voluptatem nisi facilis modi porro tenetur et itaque recusandae! Delectus.
-                                </div>
-                            </div>
-
-                        </div>
+    return (
+        <>
+          <Navbar />
+          <div className='pt-24'>
+            <div>
+              <div className='flex justify-center items-center flex-wrap'>
+                {data.items.map((item) => (
+                  <div
+                    key={item.id}
+                    className='m-10 flex flex-col justify-center items-center p-5 px-10 gap-5 flex-wrap shadow-xl rounded-3xl hover:shadow-2xl cursor-pointer max-w-sm'
+                  >
+                    <div className='bg-gradient-to-t from-gray-300 to-gray-500 w-full flex justify-center p-5'>
+                      <img src={item.img} alt={item.name} />
                     </div>
-                </div>
+                    <div className='flex flex-col items-center justify-center text-start gap-3'>
+                      <div className='text-2xl font-bold '>
+                        <h1>{item.name}</h1>
+                      </div>
+                      <div className='font-light '>{item.Description}</div>
+                    </div>
+                  </div>
+                ))}
+              </div>
             </div>
-    </>
-  )
-}
-
-export default menu
+          </div>
+        </>
+      );
+    };
+    
+    export default menu;
