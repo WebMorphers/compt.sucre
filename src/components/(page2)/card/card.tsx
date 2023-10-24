b9b1299831ec567d186065b57364447c0d7afd75
moncef <145851559+WebMorphers@users.noreply.github.com>
2023-10-24 12:25:13 +0000

    Update card.tsx

--- src/components/(page2)/card/card.tsx
+++ src/components/(page2)/card/card.tsx
@@ -1,31 +1,26 @@
-
-import './card.css'
-import { LazyLoadImage } from 'react-lazy-load-image-component';
+import "./card.css";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 type CardProps = {
-    item: {
-        img: string;
-        name: string;
-        Description: string;
-    };
+  item: {
+    img: string;
+    name: string;
+    Description: string;
+  };
 };
 
 const Card = ({ item }: CardProps) => {
-    return (
-        <div>
-            <div className="card max-md:m-5 xlg: m-12 hover:shadow-2xl rounded-3xl">
-            <LazyLoadImage
-      alt="alt" 
-      src={item.img}  
-      effect='blur'
-      placeholderSrc=''  />
-                <div className="card__content">
-                    <p className="card__title">{item.name}</p>
-                    <p className="card__description">{item.Description}</p>
-                </div>
-            </div>
+  return (
+    <div>
+      <div className="card max-md:m-5 xlg: m-12 hover:shadow-2xl rounded-3xl">
+        <LazyLoadImage alt="alt" src={item.img} effect="blur" />
+        <div className="card__content">
+          <p className="card__title">{item.name}</p>
+          <p className="card__description">{item.Description}</p>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
-export default Card
+export default Card;
