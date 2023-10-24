0d79663446747d1e892b3b0cbd44c2c3c54bac54
moncef <145851559+WebMorphers@users.noreply.github.com>
2023-10-24 13:00:35 +0000

    Update welcome.tsx

--- src/components/(page1)/welcome/welcome.tsx
+++ src/components/(page1)/welcome/welcome.tsx
@@ -1,41 +1,43 @@
-
-import spoon from "../../../assets/spoon.png" 
-import "./welcome.css"
-import '@fontsource-variable/glory';
-import '@fontsource/itim';
-
-
-
+import spoon from "../../../assets/spoon.png";
+import "./welcome.css";
+import "@fontsource-variable/glory";
+import "@fontsource/itim";
 
 const welcome = () => {
-  return ( 
-
+  return (
     <div className=" h-96 max-sm:h-[600px]  bg-gradient-to-b from-[#dfcdb9] via-white to-[#dfcdb9] w-full z-0 ">
-        <div className="absolute right-0 z-10">
-            <img className="h-96 max-sm:h-[600px]  max-md:opacity-80 " src={spoon} alt="" />
+      <div className="absolute right-0 z-10">
+        <img
+          className="h-96 max-sm:h-[600px]  max-md:opacity-80 "
+          src={spoon}
+          alt=""
+        />
+      </div>
+      <div className="container p-10 px-20 lg:px-44 z-0 text-center  flex flex-col justify-between h-full items-center">
+        <div className="title text-3xl flex flex-row gap-6">
+          <h1>Home-Baked With Love </h1>
         </div>
-        <div className="container p-10 px-20 lg:px-44 z-0 text-center  flex flex-col justify-between h-full items-center">
-            <div className="title text-3xl flex flex-row gap-6">
-                <h1>Home-Baked With Love </h1>  
-            </div>
-            <div className="bg-[#402B2A] h-10 w-[2px]"></div>
-            <div>
-                <p className="description text-xl ">
-                Chez Le Comptoir Sucré, nous mettons tout notre cœur dans nos brownies, pancakes et cookies
-                . Découvrez nos créations, délicieuses et préparées avec amour.
-                 Rejoignez notre communauté gourmande et laissez-vous tenter par nos délices faits maison,
-                  un vrai régal à chaque bouchée
-                 
-                </p>
-            </div>
-            <button className="w-[150px] bg-black h-[50px] my-3 flex items-center justify-center rounded-xl cursor-pointer relative overflow-hidden transition-all duration-500 ease-in-out shadow-md hover:scale-105 hover:shadow-lg before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-[#4b4b4b] before:to-[rgb(156,156,156)] before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-xl hover:before:left-0 text-[#fff]">
+        <div className="bg-[#402B2A] h-10 w-[2px]"></div>
+        <div>
+          <p className="description text-xl ">
+            Chez Le Comptoir Sucré, nous mettons tout notre cœur dans nos
+            brownies, pancakes et cookies . Découvrez nos créations, délicieuses
+            et préparées avec amour. Rejoignez notre communauté gourmande et
+            laissez-vous tenter par nos délices faits maison, un vrai régal à
+            chaque bouchée
+          </p>
+        </div>
+        <button className="w-[150px] bg-black h-[50px] my-3 flex items-center justify-center rounded-xl cursor-pointer relative overflow-hidden transition-all duration-500 ease-in-out shadow-md hover:scale-105 hover:shadow-lg before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-[#4b4b4b] before:to-[rgb(156,156,156)] before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-xl hover:before:left-0 text-[#fff]">
+          <a
+            target="-blank"
+            href="https://www.instagram.com/direct/t/17844604286524566"
+          >
             COMMANDER
-            </button>
-        </div>
-
+          </a>
+        </button>
+      </div>
     </div>
-    
-  )
-}
+  );
+};
 
-export default welcome
+export default welcome;
