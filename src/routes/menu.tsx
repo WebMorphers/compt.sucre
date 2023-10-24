7ce15da60ddd82b6c67e254ba6c239b323d2554b
moncef <145851559+WebMorphers@users.noreply.github.com>
2023-10-24 12:55:21 +0000

    Update menu.tsx

--- src/routes/menu.tsx
+++ src/routes/menu.tsx
@@ -74,8 +74,8 @@ const menu = () => {
                             <Card key={item.id} item={item} />
                         ))}
                      </div> 
-                     <button className="w-[150px] bg-black h-[50px] my-3 flex items-center justify-center rounded-xl cursor-pointer relative overflow-hidden transition-all duration-500 ease-in-out shadow-md hover:scale-105 hover:shadow-lg before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-[#4b4b4b] before:to-[rgb(156,156,156)] before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-xl hover:before:left-0 text-[#fff]">
-            COMMANDER
+                     <button   className="w-[150px] bg-black h-[50px] my-3 flex items-center justify-center rounded-xl cursor-pointer relative overflow-hidden transition-all duration-500 ease-in-out shadow-md hover:scale-105 hover:shadow-lg before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-[#4b4b4b] before:to-[rgb(156,156,156)] before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-xl hover:before:left-0 text-[#fff]">
+            <a href="https://www.instagram.com/direct/t/17844604286524566">COMMANDER</a>
             </button>
             </div>
           </div>
