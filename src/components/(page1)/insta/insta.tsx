67e882c09bd0100d29fbef1b7214305b64dccd15
moncef <145851559+WebMorphers@users.noreply.github.com>
2023-10-26 11:38:05 +0000

    f

--- src/components/(page1)/insta/insta.tsx
+++ src/components/(page1)/insta/insta.tsx
@@ -8,8 +8,44 @@ import vid2Thumbnail from "../../../assets/vid2Thumbnail.png";
 import vid3Thumbnail from "../../../assets/vid2Thumbnail.png";
 import vid4Thumbnail from "../../../assets/vid2Thumbnail.png";
 import "./insta.css";
+import React, { useState, useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+
+
+const VideoPlayer: React.FC<{ videoSrc: string; thumbnailSrc: string }> = ({
+  videoSrc,
+  thumbnailSrc,
+}) => {
+  const [isInView, setIsInView] = useState(false);
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+    threshold: 0.1,  
+  });
+
+  useEffect(() => {
+    if (inView) {
+      setIsInView(true);
+    }
+  }, [inView]);
+
+  return (
+    <div ref={ref}>
+      {isInView ? (
+        <HoverVideoPlayer
+          loop={true}
+          restartOnPaused
+          videoSrc={videoSrc}
+          thumbnailSrc={thumbnailSrc}
+        />
+      ) : (
+        <img src={thumbnailSrc} alt="Video Thumbnail" />
+      )}
+    </div>
+  );
+};
 
 const insta = () => {
+  
   return (
     <div className="py-8 px-20 max-sm:px-12 xl:px-36 flex flex-col justify-center items-center gap-5">
       <h1 className="text-6xl title text-center">Taste Like Never Before .</h1>
@@ -25,34 +61,11 @@ const insta = () => {
         </a>{" "}
         sur Instagram pour plus d'inspiration en brownies gourmets !
       </h1>
-      <div className="grid md:grid-cols-4 gap-9 grid-cols-2">
-      <HoverVideoPlayer
-  loop={true}
-  restartOnPaused 
-  videoSrc={vid1}
-  thumbnailSrc={vid1Thumbnail}  
-/>
-
-<HoverVideoPlayer
-  loop={true}
-  restartOnPaused 
-  videoSrc={vid2}
-  thumbnailSrc={vid2Thumbnail}  
-/>
-
-<HoverVideoPlayer
-loop={true}
-  restartOnPaused 
-  videoSrc={vid3}
-  thumbnailSrc={vid3Thumbnail}  
-/>
-
-<HoverVideoPlayer
-loop={true}
-  restartOnPaused
-  videoSrc={vid4}
-  thumbnailSrc={vid4Thumbnail}  
-/>
+       <div className="grid md:grid-cols-4 gap-9 grid-cols-2">
+        <VideoPlayer videoSrc={vid1} thumbnailSrc={vid1Thumbnail} />
+        <VideoPlayer videoSrc={vid2} thumbnailSrc={vid2Thumbnail} />
+        <VideoPlayer videoSrc={vid3} thumbnailSrc={vid3Thumbnail} />
+        <VideoPlayer videoSrc={vid4} thumbnailSrc={vid4Thumbnail} />
       </div>
     </div>
   );
